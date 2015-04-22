e82287bbf84558ab34b911bf397e89c70c6af61d
lutzer <in_the_fishtank@gmx.de>
2015-04-22 21:54:39 +0200

    added standard config

---
+++ LullabyWeb/config_default(rename!).php
@@ -0,0 +1,20 @@
+<?php
+
+    // CHANGE THESE SETTINGS TO POINT TO YOUR DATABASE 
+    define('DB_USER', "user"); // db user
+    define('DB_PASSWORD', "password"); // db password (mention your db password here)
+    define('DB_DATABASE', "database"); // database name
+    define('DB_SERVER', "localhost"); // db server
+
+    /* 
+        NO NEED TO CHANGE ANYTHING BELOW THIS LINE
+        ------------------------------------------
+    */
+    
+    define('DB_TABLE_RECORDINGS',"lullaby_recordings");
+    define('DIR_RECORDINGS', "recordings");
+    
+    
+    //ini_set('display_errors', 'On');
+    //error_reporting(E_ALL | E_STRICT);
+
