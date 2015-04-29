3679f100901b170fc844c1dc78254c13b74a860b
lutzer <in_the_fishtank@gmx.de>
2015-04-29 19:09:03 +0200

    added moon animation

--- LullabyApp/app/src/main/java/com/drl/lutz/lullabyapp/activities/MainActivity.java
+++ LullabyApp/app/src/main/java/com/drl/lutz/lullabyapp/activities/MainActivity.java
@@ -1,8 +1,11 @@
 package com.drl.lutz.lullabyapp.activities;
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import com.drl.lutz.lullabyapp.R;
 import com.drl.lutz.lullabyapp.database.LocationDatabase;
@@ -32,20 +35,45 @@ public class MainActivity extends FullscreenActivity {
     @Override
     protected void onStop() {
 
-        /*
-        //start out animation
-        View bottomLayout = findViewById(R.id.bottomLayout);
-        bottomLayout.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(), R.anim.bottom_out));
-        View topLayout = findViewById(R.id.topLayout);
-        topLayout.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(), R.anim.top_out));*/
+        this.recreate();
 
         super.onStop();
 
     }
 
+    public void onScreenClicked(View view) {
+
+        final View moon = findViewById(R.id.moon);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.scale_moon);
+
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                View layout = findViewById(R.id.mainLayout);
+                layout.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        moon.startAnimation(animation);
+
+        View moonText = findViewById(R.id.moonText);
+        moonText.setVisibility(View.GONE);
+    }
+
     public void onNextButtonClicked(View view) {
+
+
         startActivity(new Intent(this, RecorderActivity.class));
-        //startActivity(new Intent(this, LocationActivity.class));
         overridePendingTransition(R.anim.trans_in, R.anim.trans_out);
     }
 
